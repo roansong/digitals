4c37e82ca6508fc229415ac5f9ee7f2aff5a4c67
Roan Song <roansong.uct@gmail.com>
2017-04-14 00:56:24 +0200

    Prac complete. Commit before merge

--- prac2/src/Prac2.cpp
+++ prac2/src/Prac2.cpp
@@ -22,50 +22,28 @@
 
 
 #include "Prac2.h"
-#define MASK_SIZE 11
+#define MASK_SIZE 9
 #define INPUT_FILE "Data/small.jpg"
-#define OUTPUT_FILE "Data/stef9.jpg"
-#define Thread_Count 10000
+#define OUTPUT_FILE "Data/test.jpg"
+#define Thread_Count 1
 //------------------------------------------------------------------------------
 
 int golden_measure(void);
-void* medFilter(void* argument);
+void* Thread_main(void* argument);
 void bubbleSort(int * arr, int length, JPEG& Output,int y, int x);
-int golden_measure_threaded(int xmin,int xmax,int ymin,int ymax);
+void medFilter(int xmin,int xmax,int ymin,int ymax);
 
 struct param_struct{
-  JPEG * in_img;
-  JPEG * out_img;
   int xmin;
   int xmax;
   int ymin;
   int ymax;
-  int ID;
 };
 
 
-
-
-// This is each thread's "main" function.  It receives a unique ID
-void* medFilter(void* argument){
-  
-
+void* Thread_main(void* argument){
   struct param_struct *params = (struct param_struct*)argument;
-
-  // pthread_mutex_lock(&Mutex);
-  // printf("Hello from thread %d\n", params->ID);
-  // printf("xmin: %d; xmax: %d; ymin: %d; ymax: %d\n", params->xmin,params->xmax,params->ymin,params->ymax);
-  // pthread_mutex_unlock(&Mutex);
-
-
-  golden_measure_threaded(params->xmin,params->xmax,params->ymin,params->ymax);
-
- // Do some heavy calculation
-
-  // pthread_mutex_lock(&Mutex);
-  // printf("Thread %d: I QUIT!\n", params->ID);
-  // pthread_mutex_unlock(&Mutex);
-
+  medFilter(params->xmin,params->xmax,params->ymin,params->ymax);
  return 0;
 }
 //------------------------------------------------------------------------------
@@ -88,23 +66,18 @@ void bubbleSort(int * arr, int length, JPEG& Output,int y, int x){
     n = n-1;
   }
   temp = (int)length/2;
-  pthread_mutex_lock(&Mutex);
   Output.Rows[y][x] = arr[temp-1];
   Output.Rows[y][x+1] = arr[temp];
   Output.Rows[y][x+2] = arr[temp+1];
-  pthread_mutex_unlock(&Mutex);
   return;
 }
 
-int golden_measure_threaded(int xmin,int xmax,int ymin,int ymax)
+void medFilter(int xmin=0,int xmax=Input.Width-1,int ymin=0,int ymax=Input.Height-1)
 {
   int size = MASK_SIZE;
   int side = (int)(size-1)/2;
   int mask[size*size*Input.Components] = {0}; // each pixel has 3 components
-  
-  
-  tic();
-  int x,y,r,b,a,yind,xind;
+  int x,y,r,b,a;
   for(y = ymin; y <=ymax; y++){
     for(x = xmin; x <= xmax*Input.Components; x+=Input.Components){
       int mask[size*size*Input.Components] = {0};
@@ -112,13 +85,13 @@ int golden_measure_threaded(int xmin,int xmax,int ymin,int ymax)
         for(b = y-side; b <= y+side; b++){
           for(a = x-(side*Input.Components)+r; a <= x+(side*Input.Components)+r; a += Input.Components){
               if(a<0)
-                mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=Input.Rows[y][x+r];
+                mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=0;
               else if(b<0)
-                mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=Input.Rows[y][x+r];
+                mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=0;
               else if(a>=Input.Width*Input.Components)
-                mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=Input.Rows[y][x+r];
+                mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=0;
               else if(b>=Input.Height)
-                mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=Input.Rows[y][x+r];
+                mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=0;
               else
                 mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=Input.Rows[b][a];
           }
@@ -137,8 +110,7 @@ int golden_measure(void)
 
   printf("Input Height: %d\nInput Width: %d\n",Input.Height,Input.Width );
   printf("Mask size: %d (%d per pixel)\n",(int)(sizeof(mask)/sizeof(mask[0])),(int)MASK_SIZE*MASK_SIZE );
-  tic();
-  int x,y,r,b,a,yind,xind;
+  int x,y,r,b,a;
   for(y = 0; y < Input.Height; y++){
     for(x = 0; x < Input.Width*Input.Components; x+=Input.Components){
       int mask[size*size*Input.Components] = {0};
@@ -146,13 +118,13 @@ int golden_measure(void)
         for(b = y-side; b <= y+side; b++){
           for(a = x-(side*Input.Components)+r; a <= x+(side*Input.Components)+r; a += Input.Components){
               if(a<0)
-                mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=Input.Rows[y][x+r];
+                mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=0;
               else if(b<0)
-                mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=Input.Rows[y][x+r];
+                mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=0;
               else if(a>=Input.Width*Input.Components)
-                mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=Input.Rows[y][x+r];
+                mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=0;
               else if(b>=Input.Height)
-                mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=Input.Rows[y][x+r];
+                mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=0;
               else
                 mask[(b+side-y)*size*Input.Components+a+Input.Components-x]=Input.Rows[b][a];
           }
@@ -162,11 +134,6 @@ int golden_measure(void)
     }
   }
 }
-
-
-
-
-
 
 int main(int argc, char** argv){
   
@@ -183,73 +150,40 @@ int main(int argc, char** argv){
   // golden_measure();
   printf("Input Height: %d\nInput Width: %d\n",Input.Height,Input.Width );
   printf("Mask size: %d (%d per pixel)\n",(int)MASK_SIZE*MASK_SIZE*Input.Components,(int)MASK_SIZE*MASK_SIZE );
-  // golden_measure();
-  printf("Time 1 = %lg ms\n", (double)toc()/1e-3);
-  
-  
-  printf("End of SECTION 1...\n\n");
-  // End of example -------------------------------------------------------------
-
+  printf("Thread Count: %d\n", Thread_Count);
 
   tic();
 
- // Spawn threads...
- pthread_t Thread   [Thread_Count]; // pThreads structure for thread admin
- int j;
-
-
-// struct param_struct{
-//   JPEG * in_img;
-//   JPEG * out_img;
-//   int xmin;
-//   int xmax;
-//   int ymin;
-//   int ymax;
-//   int ID;
-// };
-
-
+  // Spawn threads...
+  pthread_t Thread   [Thread_Count]; // pThreads structure for thread admin
   struct param_struct structs[Thread_Count];
-  int ind0,ind1,xmin,xmax,ymin,ymax;
-  
+  int j,ind0,ind1,xmin,xmax,ymin,ymax;
+
   int perthread = Input.Height/Thread_Count;
   int leftover = Input.Height%Thread_Count;
   int temp =0;
- for(j = 0; j < Thread_Count; j++){
-    
-
+  for(j = 0; j < Thread_Count; j++){
   ind0 = temp;
   ind1 = temp+(perthread)-1;
-
   if(leftover > 0){
-
     ind1++;
     leftover--;
   }
 
   temp = ind1+1;
-
-  structs[j].in_img = &Input; 
-  structs[j].out_img = &Output;
   structs[j].xmin = 0;
   structs[j].xmax = Input.Width-1;
   structs[j].ymin = ind0;
   structs[j].ymax = ind1;
-  structs[j].ID = j;
+  pthread_create(Thread+j, 0, Thread_main, &structs[j]);
 
-  // printf("x: %d-%d; y: %d-%d\n",structs[j].xmin,structs[j].xmax,structs[j].ymin,structs[j].ymax);
-
-  pthread_create(Thread+j, 0, medFilter, &structs[j]);
-  
- }
-
+  }
 
   // Printing stuff is a critical section...
   pthread_mutex_lock(&Mutex);
   printf("Threads created :-)\n");
   printf("Thread overheads = %lg ms\n", (double)toc()/1e-3);
   pthread_mutex_unlock(&Mutex);
-
 
   tic();
  // Wait for threads to finish
@@ -260,9 +194,7 @@ int main(int argc, char** argv){
    pthread_mutex_unlock(&Mutex);
   }
  }
-
   // No more active threads, so no more critical sections required
-
   printf("Time 2 = %lg ms\n", toc()/1e-3);
 
   // Write the output image
@@ -270,7 +202,6 @@ int main(int argc, char** argv){
     printf("Cannot write image\n");
     return -3;
   }
-
   // Clean-up
   pthread_mutex_destroy(&Mutex);
   return 0;
